f42c1e5f96f3bc585b6ef1c3e1e2a0df9e96de9d
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-06-11 20:33:23 +0530

    Implement user signup functionality with validation and password hashing

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,13 @@ import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
-
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT;
 
 app.use('/api/auth', authRoutes);
+
+
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:5001');
