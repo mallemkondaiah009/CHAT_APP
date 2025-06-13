5d2a980512ffe8abc47da08176fa8c44f2c7b157
mallemkondaiah009 <114161565+mallemkondaiah009@users.noreply.github.com>
2025-06-13 18:40:36 +0530

    chore: update frontend dependencies and add Tailwind CSS configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+
 
 dotenv.config();
 
@@ -13,6 +15,7 @@ app.use(cookieParser());
 const PORT = process.env.PORT;
 
 app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
 
 
 
